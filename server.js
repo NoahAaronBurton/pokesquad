ed1e1708f17de03ce54bb2a22d29b1a1497a315d
Noah Burton <nburton115@gmail.com>
2023-09-25 20:14:16 -0600

    fixed seq bug

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 const routes = require('./controllers');
-const sequelize = require('sequelize');
+const sequelize = require('./config/connection');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
