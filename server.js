02eb92826f2729a9d7d23efe07401396e1a6c5ea
Noah Burton <nburton115@gmail.com>
2023-09-25 19:33:18 -0600

    controllers, layouts

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes); //todo: link routes
 
 app.listen(PORT, () => {console.log(`Listening on PORT ${PORT}`)})
