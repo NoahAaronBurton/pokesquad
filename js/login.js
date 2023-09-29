26c65aac7d5183f92a593c739f442c32926b1b86
BLyman <135188980+Lyman17@users.noreply.github.com>
2023-09-28 20:47:41 -0600

    added login & logout js files

--- js/login.js
+++ js/login.js
@@ -2,20 +2,20 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
-    const email = document.querySelector('#email-login').value.trim();
+    const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
+    if (username && password) {
       // Send a POST request to the API endpoint
-      const response = await fetch('/api/users/login', {
+       const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ email, password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/players');
+        document.location.replace('/');
       } else {
         if (response.status === 422) {
           const error = await response.json();
@@ -43,7 +43,7 @@ const loginFormHandler = async (event) => {
       });
   
       if (response.ok) {
-        document.location.replace('/players');
+        // document.location.replace('/');
       } else {
         const error =await response.json()
           console.log(error);
