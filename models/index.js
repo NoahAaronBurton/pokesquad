345a6600f9a434f342f65fcb9d5ddf4675b4cf13
Joe Nadherny <joenadherny@gmail.com>
2023-10-02 13:11:52 -0600

    small change defining relationships in index.js for models

--- models/index.js
+++ models/index.js
@@ -2,5 +2,13 @@ const User = require('./User');
 const Pokemon = require('./Pokemon');
 
 //todo: define relationships between tables
+User.hasMany(Pokemon, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+Pokemon.belongsTo(User, {
+    foreignKey: "user_id"
+});
 
 module.exports = {User, Pokemon};
