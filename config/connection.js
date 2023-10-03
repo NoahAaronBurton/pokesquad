ed74361b435e014a46b6eb75dad58bec8e7152c2
Noah Burton <nburton115@gmail.com>
2023-10-03 15:06:22 -0600

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,10 @@
 const Sequelize = require('sequelize');
-require('dotenv').config();
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
-}
+// Use environment variables for database connection
+const sequelize = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  dialect: 'mysql',
+});
 
 module.exports = sequelize;
