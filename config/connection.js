0c1c16872272a59fa1e1d66c0f9094500031d078
Noah Burton <nburton115@gmail.com>
2023-10-03 17:22:36 -0600

    redid j query

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,23 @@
 const Sequelize = require('sequelize');
 
-// Use the MYSQL_URL environment variable for database connection
-const sequelize = new Sequelize(process.env.MYSQL_URL, {
-  dialect: 'mysql',
-});
+let sequelize;
+
+// Use the MYSQL_URL environment variable for database connection if it exists
+if (process.env.MYSQL_URL) {
+  sequelize = new Sequelize(process.env.MYSQL_URL, {
+    dialect: 'mysql',
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
 
 module.exports = sequelize;
