a50dd28ae0652db84d1c45ea0af519395b506ac1
Noah Burton <nburton115@gmail.com>
2023-10-03 15:38:32 -0600

    mysqlurl

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,7 @@
 const Sequelize = require('sequelize');
 
-// Use environment variables for database connection
-const sequelize = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
-  host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
+// Use the MYSQL_URL environment variable for database connection
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
   dialect: 'mysql',
 });
 
